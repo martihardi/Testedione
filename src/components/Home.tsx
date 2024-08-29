67f9e1c1089aec2f32d77c020a3ec14891063daa
Matheus <matheusmj12@hotmail.com>
2024-08-29 15:43:43 -0300

    oie

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,8 @@
 
 export function Home() {
+  return(
   <div>
     Boas vindas
   </div>
+  )
 }
--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -2,8 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import '../App.css'
+import { useNavigate } from 'react-router-dom'; 
 
-export function Login() {
+ function Login() {
+    const navigate = useNavigate(); 
+
+   const handleLogin = () => {
+    console.log('Login');
+    navigate('/home');
+    
+    }
   return (
     <div className="Loginz">
     <div className="LoginBox">
@@ -20,7 +28,7 @@ export function Login() {
         />
       </div>
       <div>
-        <button className="submit">entrar</button>
+        <button onClick={handleLogin} className="submit">entrar</button>
       </div>
     </div>
     </div>
