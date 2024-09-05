3cf78d93b11e7a8fa7b5aa9a4229f3a6e2f820b6
Matheus <matheusmj12@hotmail.com>
2024-09-05 16:18:02 -0300

    1111

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,19 @@
 import './Navbar.css'
+import { useNavigate } from 'react-router-dom'
 
 function Navbar() {
+    const navigate = useNavigate();
+
     return (
         <div className="Navbox"> 
         <div className="Navtext">
      Navbar
         </div>
 
-        <button onClick={} className="submitH">home</button>
-        <button onClick={} className="submitA">about</button>
-        <button onClick={} className="submitC">contact</button>
-        <button onClick={} className="submitD">dashboard</button>
-
+        <button onClick ={() => navigate('/') } className="submitH">home</button>
+        <button onClick ={() => navigate('/about') } className="submitA">about</button>
+        <button onClick ={() => navigate('/contact') } className="submitC">contact</button>
+        <button onClick ={() => navigate('/dashboard') } className="submitD">dashboard</button>
         </div>
     )
 }
