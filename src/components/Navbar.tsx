e4694cf75399dc5778d519a927034718b640ae34
Matheus <matheusmj12@hotmail.com>
2024-09-17 18:02:39 -0300

    oi

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,19 +1,45 @@
-import './Navbar.css'
 import { useNavigate } from 'react-router-dom'
+import { LuSearch } from "react-icons/lu";
 
 function Navbar() {
     const navigate = useNavigate();
 
     return (
-        <div className="Navbox"> 
-        <div className="Navtext">
-     Navbar
-        </div>
+        <div className="bg-violet-900 h-52 flex justify-center items-center">
+            <div className='w-[80%] flex flex-row justify-between'>
+                <div>
+                    <p className='text-4xl font-bold'>The Blog</p>
+                </div>
 
-        <button onClick ={() => navigate('/home') } className="submitH">home</button>
-        <button onClick ={() => navigate('/about') } className="submitA">about</button>
-        <button onClick ={() => navigate('/contact') } className="submitC">contact</button>
-        <button onClick ={() => navigate('/dashboard') } className="submitD">dashboard</button>
+                <div>
+                    <button
+                        onClick={() => navigate('/home')}
+                        className="p-4 font-semibold">
+                        home
+                    </button>
+                    <button
+                        onClick={() => navigate('/about')}
+                        className="p-4 font-semibold">
+                        about
+                    </button>
+                    <button
+                        onClick={() => navigate('/contact')}
+                        className="p-4 font-semibold">
+                        contact
+                    </button>
+                    <button
+                        onClick={() => navigate('/dashboard')}
+                        className="p-4 font-semibold">
+                        dashboard
+                    </button>
+                </div>
+                <div className='flex items-center  '>
+                    <input type="text" placeholder='pesquisar' className=' p-2 rounded text-white' />
+                   <div className=' bg-purple400 h-10 rounded flex justify-center items-center cursor-pointer'> 
+                    <LuSearch />
+                   </div>
+                </div>
+            </div>
         </div>
     )
 }
