5b25aee1f13feb628dba6c2f06bdbc84eb7d31ef
Matheus <matheusmj12@hotmail.com>
2024-09-19 17:16:23 -0300

    rrr

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -17,18 +17,18 @@ export function Login() {
 
   
   return (
-    <div className=" flex justify-center h-screen bg-neutral-200 content-cente py-96 ">
-      <div className="bg-slate-50 justify-center content-center rounded-lg shadow-lg ">
-        <div className="LoginHeader">Login</div>
-        <div className="inputs">
+    <div className=" flex justify-center h-screen bg-neutral-200 content-center py-96 ">
+      <div className="bg-slate-50 w-96 h-96 flex flex-col justify-center content-center rounded-lg shadow-lg ">
+        <div className="target:next-center text-2xl font-bold text-purple-800 p-4">Login</div>
+        <div className="flex flex-col justify-center items-center">
           <input
-            className="User"
+            className="p-2 border border-purple-800 rounded mb-4"
             value={user}
             onChange={(e) => setUser(e.target.value)}
             placeholder="coloque seu usuario"
           />
           <input
-            className="Password"
+            className="p-2 border border-purple-800 rounded"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             placeholder="coloque sua senha"
@@ -36,7 +36,7 @@ export function Login() {
           />
         </div>
         <div>
-          <button onClick={handleLogin} className="submit">entrar</button>
+          <button onClick={handleLogin} className="w-48 border border-purple-800 m-4 p-2">entrar</button>
         </div>
       </div>
     </div>
