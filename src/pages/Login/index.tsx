428874893fe511ee856c530ca3f01a833b955a7e
Matheus <matheusmj12@hotmail.com>
2024-09-24 15:00:34 -0300

    ssssssssssssss

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -19,7 +19,7 @@ export function Login() {
   return (
     <div className=" flex justify-center h-screen bg-neutral-200 content-center py-96 ">
       <div className="bg-slate-50 w-96 h-96 flex flex-col justify-center content-center rounded-lg shadow-lg ">
-        <div className="target:next-center text-2xl font-bold text-purple-800 p-4">Login</div>
+        <div className="target:next-center text-2xl font-bold text-purple-800 p-4 align self-center">Login</div>
         <div className="flex flex-col justify-center items-center">
           <input
             className="p-2 border border-purple-800 rounded mb-4"
@@ -36,7 +36,7 @@ export function Login() {
           />
         </div>
         <div>
-          <button onClick={handleLogin} className="w-48 border border-purple-800 m-4 p-2">entrar</button>
+          <button onClick={handleLogin} className="target:next-center w-48 border border-purple-800 m-4 p-2 align self-center">entrar</button>
         </div>
       </div>
     </div>
