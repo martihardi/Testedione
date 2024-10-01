77d8f4e7ba4d4f683fbf958cb5306c792b918820
Matheus <matheusmj12@hotmail.com>
2024-10-01 16:35:12 -0300

    oi

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,17 +1,18 @@
 
+
 export const Home = () => {
     return (
-        <div className="flex align-middle bg-violet-900 p-4">
-            <div className="w-96 h-96 flex flex-col justify-center content-center ">
-            <div className=" text-4xl">
+        <div className="flex align-middle bg-violet-900 p-4" >
+            <div className="w-96 h-96 flex flex-col justify-center content-center p- ">
+            <div className=" text-4xl text-white">
                Veja o Guia definitivo para conquistar seus objetivos como dev em 2022
             </div>
-            <div className=" w-auto text-2xl">
+            <div className=" w-auto text-2xl text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend massa velit, eu pharetra nisl semper in. Fusce congue metus pharetra, porta tortor eget, vehicula massa. Curabitur tincidunt nisi eget est cursus porttitor.
             </div>
             </div>
             <div>
-               imagem
+             <img src="./assets/imagem.jpg"/>
             </div>
         </div>
     )
