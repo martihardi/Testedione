38e13f8ff2780190f493424a99f03d79f27a2397
Matheus <matheusmj12@hotmail.com>
2024-10-10 16:43:14 -0300

    2333

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,7 +1,7 @@
 export const Contact = () => {
 
     return (
-        <div>
+        <div className="bg-violet-900 text-white">
             <h1>Entre em contatos conosco:</h1>
             <a href="https://www.interfacesoftware.com.br/">Clique aqui</a>
         </div>
