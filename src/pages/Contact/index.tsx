68125fdb68106ffea83d7beb9ee545678d9ad7f2
Matheus <matheusmj12@hotmail.com>
2024-10-29 18:46:16 -0300

    22222

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,13 +1,14 @@
 export const Contact = () => {
 
     return (
-        <div className="bg-violet-900 text-white flex align-middle content-center justify-center text-4xl h-90 shadow-xl p-8">
+        <div className="bg-violet-900 flex align-middle content-center justify-center text-4xl h-90 shadow-xl p-20">
             <div>
-            <h1 className=" ">Entre em contatos conosco:</h1>
+            <h1 className=" text-white  ">Entre em contatos conosco:</h1>
             </div>
             <div>
-            <a href="https://www.interfacesoftware.com.br/"  className="p-2">Clique aqui</a>
+            <a href="https://www.interfacesoftware.com.br/"  className="p-16 text-cyan-100">Clique aqui</a>
             </div>
+            
         </div>
     )
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,12 +2,12 @@ import img from "../../assets/imagem.jpg"
 
 export const Home = () => {
     return (
-        <div className="flex align-middle content-center justify-center bg-violet-900 p-4 shadow-xl p-8" >
-            <div className="w-96 h-96 flex flex-col justify-center content-center align-middle ">
-            <div className=" text-4xl text-white ">
+        <div className="flex align-middle content-center bg-violet-900 p-4 shadow-xl flex-row justify-between " >
+            <div className="w-96 h-96 flex flex-col justify-center content-center align-middle  ">
+            <div className=" text-4xl text-white  ">
                Veja o Guia definitivo para conquistar seus objetivos como dev em 2022
             </div>
-            <div className=" w-auto text-2xl text-white">
+            <div className=" w-auto text-2xl text-white ">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec eleifend massa velit, eu pharetra nisl semper in. Fusce congue metus pharetra, porta tortor eget, vehicula massa. Curabitur tincidunt nisi eget est cursus porttitor.
             </div>
             </div>
